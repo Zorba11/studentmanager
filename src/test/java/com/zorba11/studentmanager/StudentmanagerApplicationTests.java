2fee5927cfd443d0130e771cef77d19aab691af6
Zorba11 <zorbathegreek111@gmail.com>
2021-08-27 18:24:33 -0600

    make test fail

--- src/test/java/com/zorba11/studentmanager/StudentmanagerApplicationTests.java
+++ src/test/java/com/zorba11/studentmanager/StudentmanagerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.zorba11.studentmanager;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class StudentmanagerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Oops error");
 	}
 
 }
